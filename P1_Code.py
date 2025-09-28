8b9fd3c1a377fcae8317f1b3995926ae9fd49862
Matt <134557448+mattfintech@users.noreply.github.com>
2025-09-28 15:29:20 -0700

    Create P1_Code.py

---
+++ P1_Code.py
@@ -0,0 +1,20 @@
+# 1. **BUSINESS PROBLEM**
+
+## 1. 1. **Context**
+* Urban areas face daily risks, and government agencies need data-driven insights to allocate resources and reduce them.
+
+## 1. 2. **Defining**
+* How can I reduce urban risk?
+
+## 1. 3. **Solving**
+* ETL to clean and unify data, EDA to uncover patterns, Visualizations to identify hotspots.
+
+# 2. **DATA PIPELINE**
+
+## 2. 1. **ETL**
+
+'1. Import Required Libraries'
+
+import pandas as pd
+import numpy as np
+
